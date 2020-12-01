cf143c8a972aecf1ca23376347d8bc9e51ce9f3d
Johann Poenitz <>
2020-12-01 16:42:16 +0100

    Adding Docker compose file and adding data code to both packages

--- amos/data_mart_refresher/conftest.py
+++ amos/data_mart_refresher/conftest.py
@@ -6,3 +6,8 @@ def env_setup(monkeypatch):
     monkeypatch.setenv("MTS_ENDPOINT_URL", "https://test_1.com/nothing")
     monkeypatch.setenv("ILS_ENDPOINT_URL", "https://test_2.com/something")
     monkeypatch.setenv("PYTHONPATH", "/data_mart_refresher")
+    monkeypatch.setenv("PGHOST", "test")
+    monkeypatch.setenv("PGPORT", "test")
+    monkeypatch.setenv("PGDATABASE", "test")
+    monkeypatch.setenv("PGUSER", "test")
+    monkeypatch.setenv("PGPASSWORD", "test")
