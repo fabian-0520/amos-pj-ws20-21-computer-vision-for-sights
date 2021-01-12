6a2e8f28f4de10adde95c22792ddcbcae25fdf77
nicolehmann15 <n.lehmann.1@campus.tu-berlin.de>
2021-01-12 10:40:05 +0100

    debugging12

--- amos/mts/yolov5/test/test_trainer_endpoint.py
+++ amos/mts/yolov5/test/test_trainer_endpoint.py
@@ -3,8 +3,8 @@ import unittest
 
 import yaml
 import sys
-sys.path.append('..')
-from trainer_endpoint import (
+sys.path.append('./..')
+from yolov5.trainer_endpoint import (
     generate_training_config_yaml,
     parse_label_string, persist_training_data, cleanup,
 )
