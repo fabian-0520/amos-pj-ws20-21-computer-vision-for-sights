e3f91f97b1b5649784561ebc588e285fe33d3ab0
Felix <knopp@campus.tu-berlin.de>
2021-01-14 10:46:19 +0100

    Edited Docker File, Added Files for training

--- amos/mts/yolov5/utils/plots.py
+++ amos/mts/yolov5/utils/plots.py
@@ -65,22 +65,6 @@ def plot_one_box(x, img, color=None, label=None, line_thickness=None):
         cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
         cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
 
-def plot_one_point(x, img, color=None, label=None, point_thickness=None, r=0):
-    tl = point_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
-    color = color or [random.randint(0, 255) for _ in range(3)]
-    c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
-    xMiddle = c1[0] + (c2[0]-c1[0])/2
-    yMiddle = c1[1] + (c2[1]-c1[1])/2
-    middle = (int(xMiddle), int(yMiddle))
-    #cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
-    if label:
-        tf = max(tl - 1, 1)  # font thickness
-        #t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
-        #c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
-        #cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
-        cv2.circle(img, middle, r, color=color, thickness=tl)
-        cv2.putText(img, label, (middle[0] + r + 2, middle[1] - r - 2), 0, 1, color=[255,255,255], thickness=tf * 2, lineType=cv2.LINE_AA, bottomLeftOrigin=False)
-        cv2.putText(img, label, (middle[0]+r, middle[1]-r), 0, 1, color=[0,0,0], thickness=tf*2, lineType=cv2.LINE_AA, bottomLeftOrigin = False)
 
 def plot_wh_methods():  # from utils.plots import *; plot_wh_methods()
     # Compares the two methods for width-height anchor multiplication
