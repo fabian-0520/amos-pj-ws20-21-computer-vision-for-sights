dcab784133e941260d54a7d5f8955f16930859db
Felix <knopp@campus.tu-berlin.de>
2021-01-17 22:31:55 +0100

    small fix in detect.py

--- amos/mts/yolov5/detect.py
+++ amos/mts/yolov5/detect.py
@@ -9,15 +9,12 @@ from numpy import random
 
 from models.experimental import attempt_load
 from utils.datasets import LoadStreams, LoadImages
-<<<<<<< Updated upstream
 from utils.general import check_img_size, non_max_suppression, apply_classifier, scale_coords, xyxy2xywh, \
     strip_optimizer, set_logging, increment_path
 from utils.plots import plot_one_point, plot_one_box
-=======
 from utils.general import check_img_size, check_requirements, non_max_suppression, apply_classifier, scale_coords, \
     xyxy2xywh, strip_optimizer, set_logging, increment_path
 from utils.plots import plot_one_box
->>>>>>> Stashed changes
 from utils.torch_utils import select_device, load_classifier, time_synchronized
 
 
@@ -114,15 +111,12 @@ def detect(save_img=False):
                             f.write(('%g ' * len(line)).rstrip() % line + '\n')
 
                     if save_img or view_img:  # Add bbox to image
-<<<<<<< Updated upstream
                         label = '%s %.2f' % (names[int(cls)], conf)                        
                         plot_one_point(xyxy, im0, label=label, color=colors[int(cls)], point_thickness=-1, r=20)
                         # plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
 
-=======
                         label = f'{names[int(cls)]} {conf:.2f}'
                         plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
->>>>>>> Stashed changes
 
             # Print time (inference + NMS)
             print(f'{s}Done. ({t2 - t1:.3f}s)')
