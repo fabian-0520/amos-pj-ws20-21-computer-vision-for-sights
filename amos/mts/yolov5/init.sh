9d5ac4ce1f961772c2418d31fc70062f6b17659f
Denis Koljada <denis@koljad.de>
2021-01-27 09:40:17 +0100

    adjusted strip optimizer call, added model upload

--- amos/mts/yolov5/init.sh
+++ amos/mts/yolov5/init.sh
@@ -5,6 +5,8 @@ python3 trainer_endpoint.py
 # Start training
 python3 train.py --epochs 1 --data sight_training_config.yaml --weights yolov5s.pt
 # strip optimizer that reduces weights to tmp
-python3 -c "from utils.general import *; strip_optimizer('runs/train/exp0_*/weights/best.pt', 'tmp.pt')"
+python3 -c "from utils.general import *; strip_optimizer(s='tmp.pt')"
+# upload the weights
+python3 -c "from trainer_endpoint import upload_trained_model; upload_trained_model()"
 # final clean up
 python3 -c "from trainer_endpoint import cleanup; cleanup()"
